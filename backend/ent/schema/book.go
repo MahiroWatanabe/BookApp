10e815036478c1fba7e3666787ef681e666ca2f4
MahiroWatanabe <konchikuwa2@gmail.com>
2023-10-14 11:26:12 +0900

    データベース定義

--- backend/ent/schema/book.go
+++ backend/ent/schema/book.go
@@ -1,6 +1,12 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
 
 // Book holds the schema definition for the Book entity.
 type Book struct {
@@ -9,10 +15,19 @@ type Book struct {
 
 // Fields of the Book.
 func (Book) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.Int("user_id").Positive(),
+		field.String("title").NotEmpty(),
+		field.String("body").NotEmpty(),
+		field.Time("created_at").Default(time.Now),
+		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now),
+	}
 }
 
 // Edges of the Book.
 func (Book) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		// BookからみてUserは1
+		edge.From("user", User.Type).Ref("books").Field("user_id").Unique().Required(),
+	}
 }
--- backend/ent/schema/user.go
+++ backend/ent/schema/user.go
@@ -1,6 +1,12 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
 
 // User holds the schema definition for the User entity.
 type User struct {
@@ -9,10 +15,19 @@ type User struct {
 
 // Fields of the User.
 func (User) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("email").Unique().NotEmpty(),
+		field.String("password").Sensitive().NotEmpty(),
+		field.String("name").NotEmpty(),
+		field.Time("created_at").Default(time.Now),
+		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now),
+	}
 }
 
 // Edges of the User.
 func (User) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		// Userから見てBookは多
+		edge.To("books", Book.Type),
+	}
 }
